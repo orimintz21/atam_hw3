8cae00c60640e6906f279b5fb1829ceadf5aa46b
orimintz21 <orimintz21@gmail.com>
2022-12-23 11:27:03 +0200

    first commit

---
+++ part1/hw3_part1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <syscall.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/reg.h>
+#include <sys/user.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "elf64.h"
+
+#define	ET_NONE	0	//No file type 
+#define	ET_REL	1	//Relocatable file 
+#define	ET_EXEC	2	//Executable file 
+#define	ET_DYN	3	//Shared object file 
+#define	ET_CORE	4	//Core file 
+
+
+/* symbol_name		- The symbol (maybe function) we need to search for.
+ * exe_file_name	- The file where we search the symbol in.
+ * error_val		- If  1: A global symbol was found, and defined in the given executable.
+ * 			- If -1: Symbol not found.
+ *			- If -2: Only a local symbol was found.
+ * 			- If -3: File is not an executable.
+ * 			- If -4: The symbol was found, it is global, but it is not defined in the executable.
+ * return value		- The address which the symbol_name will be loaded to, if the symbol was found and is global.
+ */
+unsigned long find_symbol(char* symbol_name, char* exe_file_name, int* error_val) {
+	//TODO: Implement.	
+	return 0;
+}
+
+int main(int argc, char *const argv[]) {
+	int err = 0;
+	unsigned long addr = find_symbol(argv[1], argv[2], &err);
+
+	if (addr > 0)
+		printf("%s will be loaded to 0x%lx\n", argv[1], addr);
+	else if (err == -2)
+		printf("%s is not a global symbol! :(\n", argv[1]);
+	else if (err == -1)
+		printf("%s not found!\n", argv[1]);
+	else if (err == -3)
+		printf("%s not an executable! :(\n", argv[2]);
+	else if (err == -4)
+		printf("%s is a global symbol, but will come from a shared library\n", argv[1]);
+	return 0;
+}
